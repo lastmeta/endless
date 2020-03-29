a3585b3e2a822dc1bfc218634f67a3f542035b8a
Jordan Miller <jordan.kay@gmail.com>
2020-03-28 23:02:34 -0600

    plan

---
+++ endless/lib/__init__.py
@@ -0,0 +1,9 @@
+1. start here
+https://hub.docker.com/r/ipfs/go-ipfs#running-ipfs-inside-docker
+
+2. use this
+https://github.com/ipfs/py-ipfs-http-client
+
+3. make demo
+transfer source code over ipfs from one endless browser image to another
+view results through mounted port via web UI
